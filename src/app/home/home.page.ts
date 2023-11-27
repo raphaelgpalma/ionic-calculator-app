15ee593f6ae179a32c9ca7a08bb182b562696648
Diogo Lepri Moreira <124713632+DiogoLepri@users.noreply.github.com>
2023-11-27 17:41:29 -0300

    Update home.page.ts
    certo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,48 +1,49 @@
-// Importando o Componente de Angular
 import { Component } from '@angular/core';
 
-// Decorator do Componente, definindo o seletor, template HTML e estilos CSS
 @Component({
-  selector: 'app-home', // Seletor usado para referenciar este componente
-  templateUrl: 'home.page.html', // Template HTML associado
-  styleUrls: ['home.page.scss'], // Arquivos de estilo associados
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  // Definindo propriedades para a lógica da calculadora
-  resultado: string = '0'; // Armazena o resultado atual ou o número sendo inserido
-  checa_operador: boolean = false; // Verifica se um operador já foi selecionado
-  comeca_segundo_elemento: boolean = false; // Indica se o segundo número (após o operador) deve começar a ser preenchido
-  resultado_concluido: boolean = false; // Indica se a operação foi concluída
-  primeiro_elemento: string = ''; // Armazena o primeiro número inserido
-  segundo_elemento: string = ''; // Armazena o segundo número inserido
-  operador_selecionado: string = ''; // Armazena o operador selecionado
-  memoria: string = ''; // Armazena a expressão completa da operação realizada
 
-  // Construtor do componente (vazio neste caso)
+  resultado: string = '0';
+  checa_operador: boolean = false;
+  comeca_segundo_elemento: boolean = false;
+  resultado_concluido: boolean = false;
+  primeiro_elemento: string = '';
+  segundo_elemento: string = '';
+  operador_selecionado: string = '';
+  memoria: string = '';
+
   constructor() { }
 
-  // Método para tratar a entrada de dígitos
   digito(valor: string) {
     if (this.resultado_concluido) {
-      // Reseta o resultado se uma operação foi concluída anteriormente
       this.resultado = valor;
       this.resultado_concluido = false;
       this.checa_operador = false;
       this.segundo_elemento = "";
     } else {
-      // Adiciona o dígito ao número correto (primeiro ou segundo)
       if (this.comeca_segundo_elemento) {
-        this.segundo_elemento += valor;
+        //preencher o segundo elemento
+        this.segundo_elemento = this.segundo_elemento + valor;
+        this.resultado = this.resultado + valor;
       } else {
-        this.resultado = this.resultado === "0" ? valor : this.resultado + valor;
+        if (this.resultado == "0") {
+          this.resultado = valor;
+        } else {
+          this.resultado = this.resultado + valor;
+        }
       }
     }
+
+
+
   }
 
-  // Método para selecionar um operador
   operador(valor: string) {
     if (!this.checa_operador) {
-      // Define o primeiro número e o operador selecionado
       this.primeiro_elemento = this.resultado;
       this.resultado += valor;
       this.checa_operador = true;
@@ -51,9 +52,7 @@ export class HomePage {
     }
   }
 
-  // Método para resetar a calculadora
   redefinir() {
-    // Reseta todos os campos e indicadores para o estado inicial
     this.resultado = "0";
     this.checa_operador = false;
     this.primeiro_elemento = '';
@@ -63,35 +62,52 @@ export class HomePage {
     this.memoria = '';
   }
 
-  // Método para calcular o resultado da expressão
   calcular() {
-    // Converte os elementos numéricos para float
-    let primeiro = parseFloat(this.primeiro_elemento);
-    let segundo = parseFloat(this.segundo_elemento);
+  let primeiro = parseFloat(this.primeiro_elemento);
+  let segundo = parseFloat(this.segundo_elemento);
 
-    // Verifica a validade dos números e executa a operação
+  
     if (isNaN(primeiro) || (isNaN(segundo) && this.operador_selecionado !== "²")) {
       this.resultado = "Erro: Entrada inválida";
+      this.resultado_concluido = true;
     } else {
       switch (this.operador_selecionado) {
-        // Executa a operação matemática com base no operador
-        case "+": this.resultado = (primeiro + segundo).toString(); break;
-        case "-": this.resultado = (primeiro - segundo).toString(); break;
-        case "*": this.resultado = (primeiro * segundo).toString(); break;
-        case "/":
-          this.resultado = segundo === 0 ? "Erro: Divisão por zero" : (primeiro / segundo).toString();
+        case "+":
+          this.resultado = (primeiro + segundo).toString();
           break;
-        case "%": this.resultado = ((primeiro * segundo) / 100).toString(); break;
-        case "²": this.resultado = (Math.pow(primeiro, 2)).toString(); break;
-        case "1/": this.resultado = (1 / segundo).toString(); break;
-        default: this.resultado = "Erro: Operador inválido";
+        case "-":
+          this.resultado = (primeiro - segundo).toString();
+          break;
+        case "*":
+          this.resultado = (primeiro * segundo).toString();
+          break;
+        case "/":
+          if (segundo === 0) {
+            this.resultado = "Erro: Divisão por zero";
+          } else {
+            this.resultado = (primeiro / segundo).toString();
+          }
+          break;
+        case "%":
+          this.resultado = ((primeiro * segundo) / 100).toString();
+          break;
+        case "²":
+          this.resultado = (Math.pow(primeiro, 2)).toString();
+          segundo = null; 
+          break;
+        case "1/":
+          this.resultado = (1 / segundo).toString();
+          primeiro = null;
+          break;
+        default:
+          this.resultado = "Erro: Operador inválido";
       }
-
-      // Atualiza a memória da calculadora com a expressão completa
+  
+      // Ajuste para lidar com casos em que o segundo não é necessário
       const segundoElementoString = segundo !== null ? segundo.toString() : "";
+  
       this.memoria = `${primeiro} ${this.operador_selecionado} ${segundoElementoString} = ${this.resultado}`;
+      this.resultado_concluido = true;
     }
-    // Indica que o resultado foi concluído
-    this.resultado_concluido = true;
   }
-}
+}  
